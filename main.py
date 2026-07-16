71d1b0d861f807cf534739d9cc9d503a68faf622
shamsiddinsultonov85-gif <shamsiddinsultonov85@gmail.com>
2026-07-17 01:02:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-
-TOKEN = "TOKEN_BU_YERGA_YOZILMAYDI"
+from config import BOT_TOKEN
+from handlers import country_keyboard
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [
-        [InlineKeyboardButton("🎟 Bilet sotib olish", url="https://SIZNING-TOLOV-LINKINGIZ")],
-        [InlineKeyboardButton("🎟 Купить билет", url="https://SIZNING-TOLOV-LINKINGIZ")]
-    ]
-
     await update.message.reply_text(
-        "Tilni tanlang / Выберите язык:",
-        reply_markup=InlineKeyboardMarkup(keyboard)
+        "🌍 Davlatingizni tanlang:",
+        reply_markup=country_keyboard()
     )
 
-app = Application.builder().token(TOKEN).build()
+app = Application.builder().token(BOT_TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
 
 print("Bot ishga tushdi...")
