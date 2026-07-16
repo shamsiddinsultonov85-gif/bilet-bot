516dfd5a14c2d28dd7b21cb286003b7e92b1e04a
shamsiddinsultonov85-gif <shamsiddinsultonov85@gmail.com>
2026-07-17 01:12:05 +0500

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,26 +1,44 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 def country_keyboard():
+
     keyboard = [
+
         [
             InlineKeyboardButton("🇹🇷 Turkey", callback_data="turkey"),
             InlineKeyboardButton("🇰🇿 Kazakhstan", callback_data="kazakhstan"),
         ],
+
         [
             InlineKeyboardButton("🇷🇺 Russia", callback_data="russia"),
             InlineKeyboardButton("🇵🇱 Poland", callback_data="poland"),
         ],
+
         [
             InlineKeyboardButton("🇹🇯 Tajikistan", callback_data="tajikistan"),
             InlineKeyboardButton("🇰🇬 Kyrgyzstan", callback_data="kyrgyzstan"),
         ],
+
         [
             InlineKeyboardButton("🇹🇲 Turkmenistan", callback_data="turkmenistan"),
             InlineKeyboardButton("🇺🇿 Uzbekistan", callback_data="uzbekistan"),
         ],
+
         [
             InlineKeyboardButton("🌍 Other Countries", callback_data="other")
         ]
+
     ]
 
     return InlineKeyboardMarkup(keyboard)
+
+
+PRICES = {
+    "turkey": "2500 ₺",
+    "kazakhstan": "21000 ₸",
+    "russia": "5000 ₽",
+    "poland": "39 €",
+    "tajikistan": "39 $",
+    "kyrgyzstan": "39 $",
+    "turkmenistan": "39 $",
+}
