3bad9009e8faade08b998ece327a82e26f5c88e2
shamsiddinsultonov85-gif <shamsiddinsultonov85@gmail.com>
2026-07-17 00:53:20 +0500

    Create countries.py

---
+++ countries.py
@@ -0,0 +1,41 @@
+COUNTRIES = {
+    "turkey": {
+        "name": "🇹🇷 Turkey",
+        "price": 2500,
+        "currency": "₺"
+    },
+    "kazakhstan": {
+        "name": "🇰🇿 Kazakhstan",
+        "price": 21000,
+        "currency": "₸"
+    },
+    "russia": {
+        "name": "🇷🇺 Russia",
+        "price": 5000,
+        "currency": "₽"
+    },
+    "poland": {
+        "name": "🇵🇱 Poland",
+        "price": 39,
+        "currency": "€"
+    },
+    "tajikistan": {
+        "name": "🇹🇯 Tajikistan",
+        "price": 39,
+        "currency": "$"
+    },
+    "kyrgyzstan": {
+        "name": "🇰🇬 Kyrgyzstan",
+        "price": 39,
+        "currency": "$"
+    },
+    "turkmenistan": {
+        "name": "🇹🇲 Turkmenistan",
+        "price": 39,
+        "currency": "$"
+    },
+    "uzbekistan": {
+        "name": "🇺🇿 Uzbekistan",
+        "blocked": True
+    }
+}
